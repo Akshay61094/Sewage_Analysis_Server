83b4fa4cee5cc3edea7ff0d384bd66288e608b9f
anupam.behera <anupam.behera@infosys.com>
2018-09-03 14:56:17 +0530

    streaming code updated

--- stream_video.py
+++ stream_video.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, Response
 import cv2
+from flask_cors import CORS
+from flask import jsonify
 
 URL='http://192.168.31.180:8080/video'
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 
@@ -27,11 +30,14 @@ def gen():
 def index():
     return render_template('index.html')
 
-
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/hello')
+def hello():
+    return jsonify("hello")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
