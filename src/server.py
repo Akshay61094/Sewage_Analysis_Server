fe4c806831d4d7c2c70960d50a30fe81dd46b3f3
anupamhap <anupamb266@gmail.com>
2018-09-10 17:41:02 +0530

    proper server code added

---
+++ src/server.py
@@ -0,0 +1,180 @@
+from flask import Flask, render_template, Response
+import cv2
+from flask_cors import CORS
+from flask import request
+from flask import jsonify
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+URL='http://192.168.31.180:8080/video'
+#
+app = Flask(__name__)
+CORS(app)
+region_selected = "Region1"
+pipe_selected = "Pipe1"
+def processVideo(inputUrl):
+    from tensorflow.python.saved_model import tag_constants
+    data_dir = './data'
+    runs_dir = './runs'
+    image_shape = (160, 576)
+    import scipy
+    import numpy as np
+    with tf.Session(graph=tf.Graph()) as sess:
+        tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], '../model')
+
+        input_img = sess.graph.get_tensor_by_name('image_input:0')
+        logits = sess.graph.get_tensor_by_name('logits:0')
+        keep_prob = sess.graph.get_tensor_by_name('keep_prob:0')
+
+        def process_image(image):
+            image = scipy.misc.imresize(image, image_shape)
+            im_softmax = sess.run(
+                [tf.nn.softmax(logits)],
+                {keep_prob: 1.0, input_img: [image]})
+            im_softmax_c = im_softmax[0][:, 0].reshape(image_shape[0], image_shape[1])
+            im_softmax_t = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+            segmentation_c = (im_softmax_c > 0.5).reshape(image_shape[0], image_shape[1], 1)
+            segmentation_t = (im_softmax_t > 0.4).reshape(image_shape[0], image_shape[1], 1)
+            mask_c = np.dot(segmentation_c, np.array([[0, 255, 0, 127]]))
+            mask_c = scipy.misc.toimage(mask_c, mode="RGBA")
+            mask_t = np.dot(segmentation_t, np.array([[255, 0, 0, 127]]))
+            mask_t = scipy.misc.toimage(mask_t, mode="RGBA")
+            street_im = scipy.misc.toimage(image)
+            street_im.paste(mask_c, box=None, mask=mask_c)
+            street_im.paste(mask_t, box=None, mask=mask_t)
+            return np.array(street_im)
+
+        from moviepy.editor import VideoFileClip
+
+        output_location = '../videos/output/'+inputUrl+'_output.mp4'
+        video_input = VideoFileClip('../videos/input/'+inputUrl+'.mp4').subclip(0,5)
+
+        video_output = video_input.fl_image(process_image)  # NOTE: this function expects color images!!
+
+        # #%time undist_clip.write_videofile(undist_output, audio=False)
+        # print("anupam")
+        video_output.write_videofile(output_location, audio=False)
+        # video_input.reader.close()
+        # video_input.audio.reader.close_proc()
+        # video_output.reader.close()
+        # video_output.audio.reader.close_proc()
+
+
+
+
+# app.config["DEBUG"] = True
+# from tensorflow.python.saved_model import tag_constants
+# data_dir = './data'
+# runs_dir = './runs'
+# image_shape = (160, 576)
+# import scipy
+# import numpy as np
+# sess = tf.Session(graph=tf.Graph())
+# # with tf.Session(graph=tf.Graph()) as sess:
+# tf.saved_model.loader.load(sess,[tf.saved_model.tag_constants.SERVING],'model')
+#
+# input_img = sess.graph.get_tensor_by_name('image_input:0')
+# logits = sess.graph.get_tensor_by_name('logits:0')
+# keep_prob =sess.graph.get_tensor_by_name('keep_prob:0')
+# def pro_image(image):
+#     image = scipy.misc.imresize(image, image_shape)
+#     im_softmax = sess.run(
+#     [tf.nn.softmax(logits)],
+#     {keep_prob: 1.0, input_img: [image]})
+#     im_softmax_c = im_softmax[0][:, 0].reshape(image_shape[0], image_shape[1])
+#     im_softmax_t = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+#     segmentation_c = (im_softmax_c > 0.5).reshape(image_shape[0], image_shape[1], 1)
+#     segmentation_t = (im_softmax_t > 0.5).reshape(image_shape[0], image_shape[1], 1)
+#     mask_c = np.dot(segmentation_c, np.array([[0, 255, 0, 127]]))
+#     mask_c = scipy.misc.toimage(mask_c, mode="RGBA")
+#     mask_t = np.dot(segmentation_t, np.array([[255, 0, 0, 127]]))
+#     mask_t = scipy.misc.toimage(mask_t, mode="RGBA")
+#     street_im = scipy.misc.toimage(image)
+#     street_im.paste(mask_c, box=None, mask=mask_c)
+#     street_im.paste(mask_t, box=None, mask=mask_t)
+#     return np.array(street_im)
+def gen(flag):
+    global region_selected
+    global pipe_selected
+    if(flag==False):
+        vidcap = cv2.VideoCapture('../videos/input/'+region_selected + pipe_selected+'.mp4')
+
+        while (True):
+            success, frame = vidcap.read()
+            plt.imshow(frame)
+            if frame is not None:
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' +cv2.imencode('.jpg', frame)[1].tostring() + b'\r\n')
+    else:
+        vidcap = cv2.VideoCapture('../videos/output/' + region_selected + pipe_selected + '_output.mp4')
+
+        while (True):
+            success, frame = vidcap.read()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tostring() + b'\r\n')
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    global region_selected
+    global pipe_selected
+    return Response(open('../videos/input/Region1Pipe1.mp4', "rb"), mimetype="video/mp4")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    region = request.args.get('region',None)
+    pipe = request.args.get('pipe',None)
+    return jsonify({'region':region,'pipe':pipe})
+
+@app.route('/input_video_feed')
+def input_video_feed():
+    global region_selected
+    global pipe_selected
+    return Response(open('../videos/input/' + region_selected + pipe_selected + '.mp4', "rb"), mimetype="video/mp4")
+
+@app.route('/output_video_feed')
+def output_video_feed():
+    global region_selected
+    global pipe_selected
+    return Response(open('../videos/output/' + region_selected + pipe_selected + '_output.mp4', "rb"), mimetype="video/mp4")
+
+
+@app.route('/process_video', methods = ['POST'])
+def postJsonHandler():
+    # print (request.is_json)
+    global region_selected
+    global  pipe_selected
+    content = request.get_json()
+    # print (content)
+    region_selected = content['region']
+    pipe_selected = content['pipe']
+    inputUrl = region_selected+pipe_selected
+    processVideo(inputUrl)
+    return jsonify('Ok')
+
+if __name__ == '__main__':
+    print("**********************hello")
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+
+    # cap = cv2.VideoCapture('http://192.168.31.180:8080/video')
+    # while (True):
+    #     # Capture frame-by-frame
+    #     ret, frame = cap.read()
+    #
+    #     # Our operations on the frame come here
+    #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #
+    #     # Display the resulting frame
+    #     cv2.imshow('frame', gray)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #
+    # # When everything done, release the capture
+    # cap.release()
+    # cv2.destroyAllWindows()
